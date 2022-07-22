7167651a7f9e3f0c555c4a6d1baf6b679820a21d
JhanU1 <jhanu@uninorte.edu.co>
2022-07-22 18:41:35 -0500

    Add: storage service

---
+++ lib/services/storage.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class Storage extends GetxService {
+  static Storage get to => Get.find();
+  final GetStorage _storage = GetStorage();
+
+  @override
+  void onInit() async {
+    await GetStorage.init();
+    super.onInit();
+  }
+
+  Future<void> save(String key, dynamic value) async {
+    await _storage.write(key, value);
+  }
+
+  Future<dynamic> read(String key) async {
+    return await _storage.read(key);
+  }
+}
